fae744d9fba7f11d8ab3c924a970ebc7a696f9fa
jha417 <jha417@sfu.ca>
2026-05-25 23:41:16 -0700

    Feature: song title edit feature. Fixed: input buffer consume.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # offline-metadata-editor
 A C++ GUI app that edits music metadata
+
+to compile:
+g++ main.cpp -I./taglib-install/include -L./taglib-install/lib -ltag -lz -o musiceditor
--- main.cpp
+++ main.cpp
@@ -2,21 +2,49 @@
 // #include "mainwindow.h"
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <limits>
+#include "taglib-install/include/taglib/fileref.h"
+#include "taglib-install/include/taglib/tag.h"
 
-void displayMainMenu() {
+void displayMainMenu()
+{
+    std::cout << "1. Edit song title" << std::endl;
+    std::cout << "2. Edit artist name" << std::endl;
+    std::cout << "3. Change song cover" << std::endl;
+    std::cout << "4. Close" << std::endl;
+    std::cout << "Type any number from \"1\" to \"4\"" << std::endl;
+}
+
+void editSongTitle(char * filename)
+{
+    TagLib::FileRef songfile(filename, true, TagLib::AudioProperties::Average);
+    std::cout << "Type new song title: ";
+    std::string song_name;
+    std::getline(std::cin, song_name);
+    songfile.tag()->setTitle(song_name);
+    if (songfile.save())
+    {
+        std::cout << "Song title changed" << std::endl;
+    };
+}
+
+void run(char * filename) 
+{
+    // Open file
+    std::ifstream ReadFile(filename);
+
     int option = -1;
     do
     {
-        std::cout << "1. Edit song title" << std::endl;
-        std::cout << "2. Edit artist name" << std::endl;
-        std::cout << "3. Change song cover" << std::endl;
-        std::cout << "4. Close" << std::endl;
-        std::cout << "Type \"1\" or \"2\"" << std::endl;
+        displayMainMenu();
         std::cin >> option;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         switch(option)
         {
             case 1:
-
+                editSongTitle(filename);
                 break;
             case 2:
 
@@ -30,26 +58,25 @@ void displayMainMenu() {
                 std::cout << "Unknown option" << std::endl;
         }
     } while (option != 4);
-}
-
-void run() {
-    // Open file
-
-    // Main menu
-   displayMainMenu();
 
     // Close file
-
+    ReadFile.close();
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[1])
 {
     // QApplication a(argc, argv);
 
     // MainWindow w;
     // w.show();
 
-    run();
+    if (argc != 2)
+    {
+        std::cerr << "error: require filename" << std::endl;
+        return -1;
+    }
+
+    run(argv[1]);
 
     // return a.exec();
     return 0;
